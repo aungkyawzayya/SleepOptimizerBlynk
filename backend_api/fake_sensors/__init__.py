5862a18d86840862f75cf9e0653398421f2abbc1
Aung <aungkyawzayya@gmail.com>
2026-05-06 16:00:26 +1200

    Fix: Removed missing humidity and co2 from fake sensors

--- backend_api/fake_sensors/__init__.py
+++ backend_api/fake_sensors/__init__.py
@@ -5,8 +5,6 @@ Simulated sensor data for testing without real hardware.
 """
 
 from .temperature import read_temperature
-from .humidity import read_humidity
-from .co2 import read_co2
 from .sound import read_sound
 from .light import read_light
 from .motion import read_motion
@@ -16,8 +14,6 @@ def read_all():
     """Read available fake sensors and return as a dict."""
     return {
         'temperature': read_temperature(),
-        'humidity': read_humidity(),
-        'co2': read_co2(),
         'sound': read_sound(),
         'light': read_light(),
         'dust': read_dust(),
